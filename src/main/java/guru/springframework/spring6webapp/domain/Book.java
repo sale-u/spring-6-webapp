422debbf92513200d14fe361a2f10aba7dd97445
sale-u <sale_u@hotmail.com>
2024-02-19 14:26:48 +0100

    2-JPA-relationship commit

--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,8 +1,8 @@
 package guru.springframework.spring6webapp.domain;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 public class Book {
 
@@ -12,6 +12,19 @@ public class Book {
     private String title;
     private String isbn;
 
+    @ManyToMany
+    @JoinTable( name="author_book",
+                joinColumns = @JoinColumn(name="book_id"),
+                inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
 
     public Long getId() {
         return id;
